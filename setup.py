96510ceee1f5e77deeb1778586008a99eb92f79f
Vaibhav Shukla <shuklavaibhav336@gmail.com>
2022-03-30 22:59:47 +0530

    backslash errors

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ os.system('cls')
 # SCRIPT.BAT
 print('==== Creating Executable Script ====')
 with open('script.bat', 'w') as file:
-    content = f'''cd /d "{CURRENT_ABSOLUTE_PATH}"\n{PYTHON_ABSOLUTE_PATH} {CURRENT_ABSOLUTE_PATH}\upload_attendance.pyc'''
+    content = f'''cd /d "{CURRENT_ABSOLUTE_PATH}"\n{PYTHON_ABSOLUTE_PATH} {CURRENT_ABSOLUTE_PATH}\\upload_attendance.pyc'''
     file.write(content)
 
 
@@ -41,8 +41,8 @@ print('Your attendances will be uploaded automatically everyday')
 print('(Enter time in railway format [HH:MM])\n')
 
 cron_runtime = input('Run Daily at: ')
-CRON_CMD_1 = f'SCHTASKS /CREATE /SC DAILY /TN "ZEUSTECH\\auto-attendance-scheduler" /TR "{CURRENT_ABSOLUTE_PATH}\upload_attendance.pyc" /ST {cron_runtime}'
-CRON_CMD_2 = f'SCHTASKS /CREATE /SC ONSTART /TN "ZEUSTECH\\update-auto-attendance" /TR "{CURRENT_ABSOLUTE_PATH}\update.pyc"'
+CRON_CMD_1 = f'SCHTASKS /CREATE /SC DAILY /TN "ZEUSTECH\\auto-attendance-scheduler" /TR "{CURRENT_ABSOLUTE_PATH}\\upload_attendance.pyc" /ST {cron_runtime}'
+CRON_CMD_2 = f'SCHTASKS /CREATE /SC ONSTART /TN "ZEUSTECH\\update-auto-attendance" /TR "{CURRENT_ABSOLUTE_PATH}\\update.pyc"'
 os.system(CRON_CMD_1)
 os.system(CRON_CMD_2)
 
@@ -53,4 +53,4 @@ print('Setup is successfully completed!\n')
 os.system('pause')
 
 if os.path.exists('update.py'):
-    os.system(f'{CURRENT_ABSOLUTE_PATH}\update.py')
+    os.system(f'{CURRENT_ABSOLUTE_PATH}\\update.py')
