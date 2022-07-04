d060d3dfd487a3c813be126800238699f9c5c8e0
Vaibhav Shukla <shuklavaibhav336@gmail.com>
2022-07-05 04:08:10 +0530

    minor changes

--- script.bat
+++ script.bat
@@ -1,2 +1,2 @@
-cd /d "E:\Zeus-Uploader-Automation-main"
-C:\easytime\Python37\python.exe E:\Zeus-Uploader-Automation-main\startup.py
+cd /d "<ABSOLUTE_PATH_TO_DIR>"
+<ABSOLUTE_PATH_TO_PYTHON> <ABSOLUTE_PATH_TO_DIR>
